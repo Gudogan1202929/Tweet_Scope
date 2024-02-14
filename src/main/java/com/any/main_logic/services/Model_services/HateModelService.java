ecf727120e0b71a7206f02604110926ab12998bb
Mohammed Buirat <1192896@student.birzeit.edu>
2024-02-15 01:18:33 +0200

    Implementing model connection services

--- src/main/java/com/any/main_logic/services/Model_services/HateModelService.java
+++ src/main/java/com/any/main_logic/services/Model_services/HateModelService.java
@@ -1,12 +1,47 @@
 package com.any.main_logic.services.Model_services;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import org.springframework.stereotype.Service;
 import com.any.model.JWT_BL.models.*;
+import com.any.model.JWT_BL.models.Enums.*;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 
 @Service
 public class HateModelService {
 
-    public Enums.OffensiveClass ClassifyText(String text){
-        return Enums.OffensiveClass.Offensive;
+    public OffensiveClass ClassifyText(String text){
+        String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+        HttpClient client = HttpClient.newHttpClient();
+        String hateEndPoint = "http://localhost:5000/predict/hate/" + encodedText;
+        try {
+            HttpRequest hateRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(hateEndPoint))
+                    .build();
+            HttpResponse<String> offensiveResponse = client.send(hateRequest, HttpResponse.BodyHandlers.ofString());
+            if (offensiveResponse.statusCode() == 200) {
+                int offensiveClass = extractClassFromJson(offensiveResponse.body());
+                OffensiveClass offensive = Enums.OffensiveClass.fromInt(offensiveClass);
+                return offensive;
+            } else {
+                System.out.println("Failed to classify offensive. Status code: " + offensiveResponse.statusCode());
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static int extractClassFromJson(String jsonBody) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(jsonBody, JsonObject.class);
+        return jsonObject.get("predicted_class").getAsInt();
     }
 }
--- src/main/java/com/any/main_logic/services/Model_services/TopicModelService.java
+++ src/main/java/com/any/main_logic/services/Model_services/TopicModelService.java
@@ -1,11 +1,45 @@
 package com.any.main_logic.services.Model_services;
-import com.any.model.JWT_BL.models.Enums;
+import com.any.model.JWT_BL.models.Enums.*;
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class TopicModelService {
 
-    public Enums.TopicClass ClassifyText(String text){
-        return Enums.TopicClass.Politics;
+    public TopicClass ClassifyText(String text){
+        String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+        HttpClient client = HttpClient.newHttpClient();
+        String topicEndpoint = "http://localhost:5000/predict/topic/" + encodedText;
+        try {
+            HttpRequest topicRequest = HttpRequest.newBuilder()
+                    .uri(URI.create(topicEndpoint))
+                    .build();
+            HttpResponse<String> topicResponse = client.send(topicRequest, HttpResponse.BodyHandlers.ofString());
+            if (topicResponse.statusCode() == 200) {
+                int topicClass = extractClassFromJson(topicResponse.body());
+                TopicClass topic = TopicClass.fromInt(topicClass);
+                return topic;
+            } else {
+                System.out.println("Failed to classify topic. Status code: " + topicResponse.statusCode());
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static int extractClassFromJson(String jsonBody) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(jsonBody, JsonObject.class);
+        return jsonObject.get("predicted_class").getAsInt();
     }
 }
