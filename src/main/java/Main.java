f5487bea3f8f3a73a1701542722abbae276e2f71
MohammadMosleh <mohammadnmosleh123@gmail.com>
2024-02-14 19:38:42 +0200

    version1

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.any.main_logic.repo.dataseed.SeedData;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SeedData.SeedData();
+        }
+}
---
+++ src/main/java/com/any/main_logic/repo/model_rep/OffensiveSummary_repo.java
@@ -0,0 +1,8 @@
+package com.any.main_logic.repo.model_rep;
+
+import com.any.model.JWT_BL.models.OffensiveSummary;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+public class OffensiveSummary_repo implements PanacheRepository<OffensiveSummary>{
+
+}
